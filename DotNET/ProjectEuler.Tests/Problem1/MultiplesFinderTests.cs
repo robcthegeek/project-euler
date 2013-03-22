e437684aa572f75b113b36e873c901e8b415b3e9
Rob Cooper <robcthegeek.public@gmail.com>
2013-03-22 11:00:33 +0000

    Add Solution to Problem 1

---
+++ DotNET/ProjectEuler.Tests/Problem1/MultiplesFinderTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using ProjectEuler.Problem1;
+
+namespace ProjectEuler.Tests.Problem1
+{
+    public class MultiplesFinderTests
+    {
+        [Test]
+        public void MultiplesOf_5LessThan10_Returns5()
+        {
+            var finder = new MultiplesFinder(5);
+            var results = finder.FindForLessThan(10);
+            Assert.AreEqual(1, results.Count);
+            Assert.Contains(5, results);
+        }
+        
+        [Test]
+        public void MultiplesOf_2or5LessThan6_Returns_2_4_5()
+        {
+            var finder = new MultiplesFinder(2, 5);
+            var results = finder.FindForLessThan(6);
+            Assert.AreEqual(3, results.Count);
+            Assert.Contains(2, results);
+            Assert.Contains(4, results);
+            Assert.Contains(5, results);
+        }
+
+        [Test]
+        public void MultiplesOf_3or5LessThan10_Returns_3_5_6_9()
+        {
+            var finder = new MultiplesFinder(3, 5);
+            var results = finder.FindForLessThan(10);
+            Assert.AreEqual(4, results.Count);
+            Assert.Contains(3, results);
+            Assert.Contains(5, results);
+            Assert.Contains(6, results);
+            Assert.Contains(9, results);
+        }
+        
+        [Test]
+        public void MultiplesOf_3or5LessThan10_Returns_Sum23()
+        {
+            var finder = new MultiplesFinder(3, 5);
+            var results = finder.FindForLessThan(10);
+            Assert.AreEqual(23, results.Sum());
+        }
+        
+        [Test]
+        public void MultiplesOf_3or5LessThan1000_Returns_THE_ANSWER()
+        {
+            var finder = new MultiplesFinder(3, 5);
+            var results = finder.FindForLessThan(1000);
+
+            Console.Write(results.Sum());
+            // Returns: 233168
+        }
+    }
+}
---
+++ DotNET/ProjectEuler/Problem1/MultiplesFinder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler.Problem1
+{
+    public class MultiplesFinder
+    {
+        private readonly List<int> _multiplesToFind;
+
+        public MultiplesFinder(params int[] multiplesToFind)
+        {
+            _multiplesToFind = multiplesToFind.ToList();
+        }
+
+        public List<int> FindForLessThan(int value)
+        {
+            var results = new List<int>();
+
+            for (int i = 1; i < value; i++)
+            {
+                if (i.IsMultipleOf(_multiplesToFind.ToArray()))
+                    results.Add(i);
+            }
+
+            return results;
+        }
+    }
+
+    internal static class IntExtensions
+    {
+        internal static bool IsMultipleOf(this int i, int value)
+        {
+            return i%value == 0;
+        }
+
+        internal static bool IsMultipleOf(this int value, params int[] any)
+        {
+            return any.Any(multiple => value.IsMultipleOf(multiple));
+        }
+    }
+}
